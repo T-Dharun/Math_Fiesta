a2cbffdf745e4f138671c67e277753917fb7fa18
satheesh1022005 <satheesh1022005@gmail.com>
2024-03-09 20:36:29 +0530

    updated ui

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -1,49 +1,116 @@
-import React, { useState, useEffect } from "react";
-import "./gallery.css"; // Add your custom styles for the carousel
-import img1 from "../../assets/gallery/pic1.JPG";
-import img2 from "../../assets/gallery/pic2.JPG";
-import img3 from "../../assets/gallery/pic3.JPG";
-import img4 from "../../assets/gallery/pic4.JPG";
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import img1 from "../../assets/gallery/img1.JPG";
+import img2 from "../../assets/gallery/img2.jpg";
+import img3 from "../../assets/gallery/img3.jpg";
+import img4 from "../../assets/gallery/img4.jpg";
+import img5 from "../../assets/gallery/img5.jpg";
+import img6 from "../../assets/gallery/img6.jpg";
+import img7 from "../../assets/gallery/img7.jpg";
+import img8 from "../../assets/gallery/img8.jpg";
+import img9 from "../../assets/gallery/img9.jpg";
+import img10 from "../../assets/gallery/img10.JPG";
+import img11 from "../../assets/gallery/img11.JPG";
+import img12 from "../../assets/gallery/img12.JPG";
+import img13 from "../../assets/gallery/img13.JPG";
+import img14 from "../../assets/gallery/img14.JPG";
+import img15 from "../../assets/gallery/img15.JPG";
+import img16 from "../../assets/gallery/img16.JPG";
+import img17 from "../../assets/gallery/img17.JPG";
+import img18 from "../../assets/gallery/img18.JPG";
+import img19 from "../../assets/gallery/img19.JPG";
 
-const Gallery = () => {
-  const images = [img1, img2, img3, img4];
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      handleNext();
-    }, 2000);
-
-    return () => clearInterval(interval);
-  }, [currentImageIndex]);
-
-  const handlePrev = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === 0 ? images.length - 1 : prevIndex - 1
-    );
-  };
-
-  const handleNext = () => {
-    setCurrentImageIndex((prevIndex) =>
-      prevIndex === images.length - 1 ? 0 : prevIndex + 1
-    );
-  };
-
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+import "./gallery.css";
+// import required modules
+import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
+function Gallery() {
   return (
-    <>
-      <h1 className="gallery-title">Gallery</h1>
-      <div className="carousel-container">
-        {images.map((img, index) => (
-          <img
-            key={index}
-            src={img}
-            alt={`Image ${index + 1}`}
-            className={index === currentImageIndex ? "active" : ""}
-          />
-        ))}
-      </div>
-    </>
+    <div>
+      <h1 className="gallery-title">Math Fiesta ' 23</h1>
+      <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        autoplay={{
+          delay: 2500,
+        }}
+        loop={true}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination, Autoplay]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src={img1} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img2} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img3} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img4} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img5} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img6} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img7} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img8} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img9} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img10} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img11} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img12} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img13} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img14} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img15} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img16} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img17} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img18} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={img19} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
-};
-
+}
 export default Gallery;
