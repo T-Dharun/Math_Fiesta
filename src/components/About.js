61ddb982010377ce3fecad9e71ac3d92768e1d32
T-Dharun <dharuneee007@gmail.com>
2024-03-09 09:40:36 +0530

    Updates

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ function About(){
                     <h1>Srinivasa Ramanujan Mathematics Club</h1>
                     <img src={Math} />
                     <aside className='reveal'>Srinivasa Ramanujan Mathematics Club was started in the year 2014-15. The club is unique in its own way. This club aims students to be faster and accurate in their works and apply Math in daily life situations. The club is functioned by students representing as main bodies such as secretary, treasurer, chairman and executive members.</aside>
-                    <aside>The club conducts many events such as Quiz, Sudoko and puzzle solving, Math Fiesta, Math pi-Thons and so on, to enhance the Student's Math and Problem Solving skills. Math Fiesta, A National Level Mathemical Symposium is one of the most popular event of the club,which is conducted each year.</aside>
+                    <aside>The club conducts many events such as Quiz , Sudoko and puzzle solving, Math Fiesta, Math pi-Thons and so on, to enhance the Student's Math and Problem Solving skills. Math Fiesta, A National Level Mathemical Symposium is one of the most popular event of the club,which is conducted each year.</aside>
                 </article>
                 <article>
                 <h1>Math Fiesta ' 24</h1>
